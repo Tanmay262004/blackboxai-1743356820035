54c83a352b8c4f75342ab7178841959a1520857f
Tanmay262004 <tanmay.navhade@cybotronics.com>
2025-03-30 23:17:24 +0530

    Add figma-desktop-app/main.js 1743356843661

---
+++ figma-desktop-app/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+// Enable live reload during development
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+})
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
